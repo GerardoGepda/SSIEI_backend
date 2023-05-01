23238fc380cb7474d64a9613999e126ebc8f3028
GerardoGepda <gerardo.gepda@gmail.com>
2023-05-01 15:11:59 -0600

    Acces controll CORS

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 //require_once "";
 include_once 'Controller/ActivoController.php';
 include_once 'Controller/MarcaController.php';
